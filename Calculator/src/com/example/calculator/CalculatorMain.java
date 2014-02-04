35cdadccb95d01b848b9387c7674b1300b1ecc03
andre.luis <andre.luis@ati.com.br>
2014-02-04 13:52:13 -0200

    Alteracoes nas funcoes da calculadora

--- Calculator/src/com/example/calculator/CalculatorMain.java
+++ Calculator/src/com/example/calculator/CalculatorMain.java
@@ -10,6 +10,10 @@ import android.widget.EditText;
 public class CalculatorMain extends Activity {
 
 	EditText editText;
+	String operator;
+	double valX = 0;
+	static int opcounter = 0;
+	static int op = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +34,40 @@ public class CalculatorMain extends Activity {
 		String val = editText.getText().toString();
 		editText.setText(val+btn.getText().toString());
 	}
-
+	
+	public void calculate(View view){
+		if (opcounter == 0) {
+			valX = Double.parseDouble(editText.getText().toString());
+			if (R.id.btnPlus == view.getId()) {
+				op = 1;
+			}else if (R.id.btnMinus == view.getId()) {
+				op = 2;
+			}else if (R.id.btnMult == view.getId()) {
+				op = 3;
+			}else if (R.id.btnDiv == view.getId()) {
+				op = 4;
+			}
+			opcounter = 1;
+			editText.setText("");
+		} else if (opcounter == 1) {
+			if (op == 1) {
+				valX = Double.parseDouble(editText.getText().toString()) + valX;
+				op = 0;
+				editText.setText(Double.toString(valX));
+			} else if (op == 2) {
+				valX = Double.parseDouble(editText.getText().toString()) - valX;
+				op = 0;
+				editText.setText(Double.toString(valX));
+			} else if (op == 3) {
+				valX = Double.parseDouble(editText.getText().toString()) * valX;
+				op = 0;
+				editText.setText(Double.toString(valX));
+			} else if (op == 4) {
+				valX = Double.parseDouble(editText.getText().toString()) / valX;
+				op = 0;
+				editText.setText(Double.toString(valX));
+			}
+			opcounter = 0;
+		}
+	}
 }
